3a940636c5d3bc146e9f418de4e07f4caee89a03
Alfredo Amengual <amengual.alfredo@gmail.com>
2016-07-29 09:26:58 -0500

    additional image functionality

--- src/components/search.js
+++ src/components/search.js
@@ -35,24 +35,22 @@ class Search extends React.Component {
         <input type="search" placeholder="What playlist?" ref="playlist"  />
         <input type="submit" value="Search on!" />
       </form>
-        <button onClick={ this._doSearch.bind(this, 'romantic dinner') }  href="#" ref="hiking"><img className="image-button" src="./images/romantic-dinner.jpg" /></button>
-        <button onClick={ this._doSearch.bind(this, 'performing arts') } href="#" ref="hiking"><img className="image-button" src="./images/performing-arts.jpg" /></button>
-        <button onClick={ this._doSearch.bind(this, 'farmers market') } href="#" ref="hiking"><img className="image-button" src="./images/farmers.jpg" /></button>
-        <button onClick={ this._doSearch.bind(this, 'brewery') } href="#" ref="hiking"><img className="image-button" src="./images/beer.jpg" /></button>
-        <button onClick={ this._doSearch.bind(this, 'hiking') } href="#" ref="hiking"><img className="image-button" src="./images/outdoors.jpg" /></button>
-        <button onClick={ this._doSearch.bind(this, 'dancing') } href="#" ref="hiking"><img className="image-button" src="./images/dancing.jpg" /></button>
-        <button onClick={ this._doSearch.bind(this, 'movies') } href="#" ref="hiking"><img className="image-button" src="./images/movies.jpg" /></button>
-        <button onClick={ this._doSearch.bind(this, 'relaxing') } href="#" ref="hiking"><img className="image-button" src="./images/relaxing.jpg" /></button>
-        <button onClick={ this._doSearch.bind(this, 'dive bar') } href="#" ref="hiking"><img className="image-button" src="./images/dive-bar.jpg" /></button>
-        <button onClick={ this._doSearch.bind(this, 'staycation') } href="#" ref="hiking"><img className="image-button" src="./images/staycation.jpg" /></button>
-        <button onClick={ this._doSearch.bind(this, 'brunch') } href="#" ref="hiking"><img className="image-button" src="./images/brunch.jpg" /></button>
-        <button onClick={ this._doSearch.bind(this, 'arts') } href="#" ref="hiking"><img className="image-button" src="./images/art-class.jpg" /></button>
-        <button onClick={ this._doSearch.bind(this, 'blind date') } href="#" ref="hiking"><img className="image-button" src="./images/blind-date.jpg" /></button>
-        <button onClick={ this._doSearch.bind(this, 'adventure') } href="#" ref="hiking"><img className="image-button" src="./images/adventure.jpg" /></button>
-        <button onClick={ this._doSearch.bind(this, 'winery') } href="#" ref="hiking"><img className="image-button" src="./images/winery.jpg" /></button>
-        <button onClick={ this._doSearch.bind(this, 'comedy') } href="#" ref="hiking"><img className="image-button" src="./images/comedy.jpg" /></button>
-
-
+        <button  onClick={ this._doSearch.bind(this, 'romantic dinner') }  href="#" ref="hiking"><img className="image-button" src="./images/romantic-dinner.jpg" /></button>
+        <button  onClick={ this._doSearch.bind(this, 'performing arts') } href="#" ref="hiking"><img className="image-button" src="./images/performing-arts.jpg" /></button>
+        <button  onClick={ this._doSearch.bind(this, 'farmers market') } href="#" ref="hiking"><img className="image-button" src="./images/farmers.jpg" /></button>
+        <button  onClick={ this._doSearch.bind(this, 'brewery') } href="#" ref="hiking"><img className="image-button" src="./images/beer.jpg" /></button>
+        <button  onClick={ this._doSearch.bind(this, 'hiking') } href="#" ref="hiking"><img className="image-button" src="./images/outdoors.jpg" /></button>
+        <button  onClick={ this._doSearch.bind(this, 'dancing') } href="#" ref="hiking"><img className="image-button" src="./images/dancing.jpg" /></button>
+        <button  onClick={ this._doSearch.bind(this, 'movies') } href="#" ref="hiking"><img className="image-button" src="./images/movies.jpg" /></button>
+        <button  onClick={ this._doSearch.bind(this, 'relaxing') } href="#" ref="hiking"><img className="image-button" src="./images/relaxing.jpg" /></button>
+        <button  onClick={ this._doSearch.bind(this, 'dive bar') } href="#" ref="hiking"><img className="image-button" src="./images/dive-bar.jpg" /></button>
+        <button  onClick={ this._doSearch.bind(this, 'staycation') } href="#" ref="hiking"><img className="image-button" src="./images/staycation.jpg" /></button>
+        <button  onClick={ this._doSearch.bind(this, 'brunch') } href="#" ref="hiking"><img className="image-button" src="./images/brunch.jpg" /></button>
+        <button  onClick={ this._doSearch.bind(this, 'arts') } href="#" ref="hiking"><img className="image-button" src="./images/art-class.jpg" /></button>
+        <button  onClick={ this._doSearch.bind(this, 'blind date') } href="#" ref="hiking"><img className="image-button" src="./images/blind-date.jpg" /></button>
+        <button  onClick={ this._doSearch.bind(this, 'adventure') } href="#" ref="hiking"><img className="image-button" src="./images/adventure.jpg" /></button>
+        <button  onClick={ this._doSearch.bind(this, 'winery') } href="#" ref="hiking"><img className="image-button" src="./images/winery.jpg" /></button>
+        <button  onClick={ this._doSearch.bind(this, 'comedy') } href="#" ref="hiking"><img className="image-button" src="./images/comedy.jpg" /></button>
     </div>
     )
   }
